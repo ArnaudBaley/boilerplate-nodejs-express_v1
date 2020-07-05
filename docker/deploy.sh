eac30cb7337bcc2cf988c82c9eed75b13285a8af
Arnaud <>
2020-07-05 18:14:35 +0200

    [DOCKER] Improve deployment script.

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -5,8 +5,8 @@
 # Use current folder to execute next commands.
 cd "$(dirname "$0")"
 
-# Build image
-docker build -t template-nodejs-express -f ../docker/Dockerfile ../
+# Remove eventual existing container.
+docker stop template_nodejs_express ; docker rm template_nodejs_express
 
-# Run image
-docker run -p 3050:3000 -d template-nodejs-express
+# Run image.
+docker-compose up -d --build
