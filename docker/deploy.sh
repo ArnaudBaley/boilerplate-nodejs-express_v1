1d4acd71e46328844ef17718678abd6febe4666f
Arnaud <>
2020-07-12 14:57:39 +0200

    [DOCKER] Fix build context issues.

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -8,5 +8,8 @@ cd "$(dirname "$0")"
 # Remove eventual existing container.
 docker stop template_nodejs_express ; docker rm template_nodejs_express
 
+# Build image (execution context is parent folder)
+docker build -t template-nodejs-express -f ../docker/Dockerfile ../
+
 # Run image.
-docker-compose up -d --build
+docker-compose up -d
