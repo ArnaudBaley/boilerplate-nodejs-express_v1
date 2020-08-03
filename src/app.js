8f3060351e8ce1c0bab060168095027fa9dc2601
Arnaud <>
2020-08-03 17:40:25 +0200

    [LOG] Replace bunyan express logger by Morgan.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { log } = require("./config/config.logger");
-const { expressLog } = require("./config/config.logger");
+const {
+  morganStdoutLogger,
+  morganFileLogger,
+} = require("./config/config.logger");
 // Import all routes from config.
 const routes = require("./config/config.routes");
 
 const app = express();
 
-// Specific log of express.
-app.use(expressLog);
+// stout logger for HTTP request.
+app.use(morganStdoutLogger);
+app.use(morganFileLogger);
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
--- src/config/config.logger.js
+++ src/config/config.logger.js
@@ -1,13 +1,15 @@
 /**
  * @summary Logger configuration.
  */
-
+const fs = require("fs");
+const path = require("path");
 const bunyan = require("bunyan");
 const bformat = require("bunyan-format");
+const morgan = require("morgan");
 
 const formatOut = bformat({ outputMode: "short" });
 
-// Application logs.
+// Custom application logs.
 const log = bunyan.createLogger({
   name: "template-nodejs-express",
   streams: [
@@ -22,22 +24,18 @@ const log = bunyan.createLogger({
   ],
 });
 
-// Express dedicated logs.
-const expressLog = require("express-bunyan-logger")({
-  name: "template-nodejs-express.log",
-  streams: [
-    {
-      level: "debug",
-      stream: formatOut, // log DEBUG and above to stdout
-    },
-    {
-      level: "error",
-      path: "/var/log/template-nodejs-express.log", // log ERROR to a file
-    },
-  ],
+// stout logger for HTTP request.
+const morganStdoutLogger = morgan("tiny");
+
+// file logger for HTTP request.
+const morganFileLogger = morgan("common", {
+  stream: fs.createWriteStream(path.join("/var/log/", "access.log"), {
+    flags: "a",
+  }),
 });
 
 module.exports = {
   log,
-  expressLog,
+  morganStdoutLogger,
+  morganFileLogger,
 };
