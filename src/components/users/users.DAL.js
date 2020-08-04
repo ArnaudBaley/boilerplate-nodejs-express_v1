d0d4a96466204b308824e9b2daa91b2255fa34ce
Arnaud <>
2020-08-04 21:11:39 +0200

    WIP mongo.

--- src/components/users/users.DAL.js
+++ src/components/users/users.DAL.js
@@ -1,3 +1,20 @@
 /**
  * @summary Users Data Access Layer (DAL).
  */
+const { log } = require("../../config/config.logger");
+const { userModel } = require("./users.model");
+
+const createUser = (user) => {
+  userModel
+    .save(user)
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      log.error(err);
+    });
+};
+
+module.exports = {
+  createUser,
+};
--- src/components/users/users.controller.js
+++ src/components/users/users.controller.js
@@ -1,11 +1,17 @@
 /**
  * @summary Users Controller.
  */
+const { log } = require("../../config/config.logger");
+const { createUser } = require("./users.DAL");
+const { userModel } = require("./users.model");
 
 async function create(req, res) {
   try {
+    const user = new userModel(req.body);
+    createUser(user);
     return res.status(201).json({ result: "OK" });
-  } catch (e) {
+  } catch (err) {
+    log.error(err);
     return res.status(500);
   }
 }
