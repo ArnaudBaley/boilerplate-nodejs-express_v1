e2fbb5a4c580dea73b1e32c7c386c35cdb6c3114
Arnaud <>
2020-08-22 16:14:37 +0200

    [OpenAPI] Add OpenAPI validator.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
+const { OpenApiValidator } = require("express-openapi-validator");
 const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 const { log } = require("./config/config.logger");
 const { connectToDB } = require("./config/config.bd");
 const {
   morganStdoutLogger,
   morganFileLogger,
 } = require("./config/config.logger");
-const swaggerDocument = require("./swagger.json");
+
 // Import all routes from config.
 const routes = require("./config/config.routes");
 
 const app = express();
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-log.info("OpenAPI UI : http://localhost:3000/api-docs");
 
 // stout logger for HTTP request.
 app.use(morganStdoutLogger);
@@ -28,13 +28,37 @@ app.use(bodyParser.json());
 // parse requests application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes.
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+const apiSpec = YAML.load(path.join(__dirname, "api.yaml"));
 
-app.listen(3000, function () {
-  log.info("Example app listening on port 3000!");
-});
+// Serve OpenAPI UI (https://www.npmjs.com/package/swagger-ui-express)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiSpec));
+log.info("OpenAPI UI : http://localhost:3000/api-docs");
 
-app.use("/api", routes.router);
+// (optionally) Serve the OpenAPI spec
+app.use("/spec", express.static(path.join(__dirname, "api.yaml")));
+
+// OpenAPI validator (https://www.npmjs.com/package/express-openapi-validator)
+new OpenApiValidator({
+  apiSpec,
+  validateResponses: true,
+})
+  .install(app)
+  .then(
+    () => {
+      log.info("OpenAPI Validator loaded.");
+
+      // Routes.
+      app.get("/", function (req, res) {
+        res.send("Hello World!");
+      });
+
+      app.use("/v1", routes.router);
+
+      app.listen(3000, function () {
+        log.info("Example app listening on port 3000!");
+      });
+    },
+    (error) => {
+      log.error(error);
+    }
+  );
