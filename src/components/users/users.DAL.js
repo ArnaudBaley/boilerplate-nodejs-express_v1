e213e97d32626ea4131556bbb81c4e64226ca24f
Arnaud <>
2020-08-30 17:10:46 +0200

    Add model validation (ajv).

--- src/components/users/users.DAL.js
+++ src/components/users/users.DAL.js
@@ -1,20 +1,29 @@
 /**
  * @summary Users Data Access Layer (DAL).
  */
+const Ajv = require("ajv");
 const { getDBClient } = require("../../config/config.bd");
 const { log } = require("../../config/config.logger");
+const userSchema = require("./users.schema.json");
 
-const createUser = async (user) => {
+const createUser = async (userData) => {
+  const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+  const validate = ajv.compile(userSchema);
+  const valid = validate(userData);
+  if (!valid) {
+    throw new Error(JSON.stringify(validate.errors));
+  }
+
   try {
     const db = getDBClient();
     const userCollection = db.collection("users");
 
     await userCollection.insert({
-      username: user.username,
+      username: userData.userName,
       createdAt: new Date(),
     });
 
-    log.debug(`création user "${user.username}" OK.`);
+    log.debug(`création user "${userData.userName}" OK.`);
   } catch (error) {
     log.error(`Database error : ${error}`);
     throw error;
